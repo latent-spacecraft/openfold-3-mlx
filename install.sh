7b6f54115c824ed438ffe9b723dc759ff00a211c
Geoffrey Taghon <geoff.taghon@gmail.com>
2025-11-16 15:11:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
+# Copyright 2025 Geoffrey Taghon
+
+echo ""
+echo "================================================================="
+echo "Openfold3-MLX Installation script - v0.1.0"
+echo "================================================================="
+echo ""
 
 # Install Openfold3-MLX
 python3 -m venv .venv
 source ./.venv/bin/activate && pip install -e .
 
-# Install dependencies
-echo ""
-echo "Installing kalign for inference..."
-source ./.venv/bin/activate && pip install git+https://github.com/TimoLassmann/kalign.git
-
 echo ""
 echo "================================================================="
 echo "Openfold3-MLX Installation script completed."
 echo "Test protein inference with: ./predict.sh <sequence> completed."
+echo "================================================================="
 echo ""
